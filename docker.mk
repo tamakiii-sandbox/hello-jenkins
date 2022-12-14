1dc739f3b8e28b9c7b4f9825ead0875699fa2b5f
tamakiii <tamakiii@users.noreply.github.com>
2022-12-14 23:01:06 +0900

    fixes

--- docker.mk
+++ docker.mk
@@ -3,17 +3,25 @@
 IMAGE := tamakiii-sandbox/hello-jenkins
 CONTAINER ?= $(shell docker ps --quiet --filter ancestor=$(IMAGE))
 
+PORT_HTTP := 8080
+PORT_AGENT := 50000
+
 help:
 	@cat $(firstword $(MAKEFILE_LIST))
 
 info:
-	docker exec -it  $(CONTAINER) cat /var/jenkins_home/secrets/initialAdminPassword
+	docker exec -it $(CONTAINER) cat /var/jenkins_home/secrets/initialAdminPassword
 
 build: Dockerfile
 	docker build -t $(IMAGE) .
 
 run: build
-	docker run -p 8080:8080 -p 50000:50000 --restart=on-failure -v jenkins_home:/var/jenkins_home $(IMAGE)
+	docker run \
+		-p $(PORT_HTTP):8080 \
+		-p $(PORT_AGENT):50000 \
+		--restart=on-failure \
+		-v $(PWD)/var/jenkins_home:/var/jenkins_home \
+		$(IMAGE)
 
 bash:
 	docker run -it --rm $(IMAGE) $@
